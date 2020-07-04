78f01e269119dac5dbe640de7ed1833522429357
Alex Maslovsky <a.maslovsky.home@gmail.com>
2020-07-04 18:27:06 +0300

    asd

--- bundle.js
+++ bundle.js
@@ -13,6 +13,10 @@ function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj;
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('/service-worker.js');
+}
+
 document.addEventListener('DOMContentLoaded', () => {
   const {
     phone: isPhone
